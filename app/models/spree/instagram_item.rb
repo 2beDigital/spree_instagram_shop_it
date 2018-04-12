c5c26ed72cae4beea1b520069ccb39ce7c7a3afb
Noel <noel@2bedigital.com>
2018-04-12 13:46:01 +0100

    fixes in backend

--- app/models/spree/instagram_item.rb
+++ app/models/spree/instagram_item.rb
@@ -1,6 +1,6 @@
 module Spree
 	class InstagramItem < ActiveRecord::Base
-			belongs_to :instagram_post
+		belongs_to :instagram_post
 		has_and_belongs_to_many :products
 		validates :image_url, presence: true
 		validates :link, presence: true
