87ef4057841ecfc539435e0afe0b105ce671401b
Noel <noel@2bedigital.com>
2018-05-10 10:20:12 +0100

    bug when rollback in migration

--- db/migrate/20180507083635_convert_spree_instagram_posts_to_utf8mb4.rb
+++ db/migrate/20180507083635_convert_spree_instagram_posts_to_utf8mb4.rb
@@ -1,9 +1,12 @@
 class ConvertSpreeInstagramPostsToUtf8mb4 < ActiveRecord::Migration
-  def change
+  def up
     # for each table that will store unicode execute:
     execute "ALTER TABLE spree_instagram_posts CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
     # for each string/text column with unicode content execute:
     execute "ALTER TABLE spree_instagram_posts CHANGE description description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;"
     execute "ALTER TABLE spree_instagram_posts CHANGE username username VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;"
+  end
+
+  def down
   end
 end
