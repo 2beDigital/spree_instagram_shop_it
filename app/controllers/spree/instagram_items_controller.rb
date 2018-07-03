564e08fecc55c113139da3c3d1cd63eb83968401
Noel <noel@2bedigital.com>
2018-07-03 05:25:06 +0100

    change order items in index

--- app/controllers/spree/instagram_items_controller.rb
+++ app/controllers/spree/instagram_items_controller.rb
@@ -1,7 +1,7 @@
 module Spree
 	class InstagramItemsController < Spree::StoreController
 		def index
-			@collection = Spree::InstagramItem.all.page(params[:page]).per(SpreeInstagramShopIt::Configuration.item_per_page)
+			@collection = Spree::InstagramItem.all.order(created_at: :desc).page(params[:page]).per(SpreeInstagramShopIt::Configuration.item_per_page)
 		end
 	end
 end
